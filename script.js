fe3bbe192e7f2817eed8b7ee96d1c4d9fa741349
Elder <elderoliveiracastro@gmail.com>
2022-06-01 00:30:31 -0300

    Adding items dynamic on the table

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const firstNumber = document.getElementById("firstNumber");
 const secondNumber = document.getElementById("secondNumber");
 const btnSum = document.getElementById("btnSum");
 const result = document.getElementById("result");
+const myTable = document.getElementById("myTable");
+
 
 btnSum.addEventListener("click", () => {
     const number1 = parseFloat(firstNumber.value);
     const number2 = parseFloat(secondNumber.value);
     result.innerText = `The result is: ${number1 + number2}`;
+
+    let row = myTable.insertRow(1);
+    let colSum = row.insertCell(0);
+    let colResult = row.insertCell(1);
+
+    colSum.innerText = `${firstNumber.value} + ${secondNumber.value}`;
+    colResult.innerText = `${number1 + number2}`; 
 });
  
